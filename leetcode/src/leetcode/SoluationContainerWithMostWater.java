41b4a0c51992ce158af8283d86bbda253f806ce1
GilbertHou <jbhouc@163.com>
2018-06-09 11:00:06 +0800

    https://leetcode-cn.com/problems/container-with-most-water/description/

---
+++ leetcode/src/leetcode/SoluationContainerWithMostWater.java
@@ -0,0 +1,37 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements. See the NOTICE
+ * file distributed with this work for additional information regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package leetcode;
+
+/**
+ * ClassName:SoluationContainerWithMostWater <br/>
+ * Function: TODO ADD FUNCTION. <br/>
+ * Reason: TODO ADD REASON. <br/>
+ * Date: 2018年6月9日 上午10:32:19 <br/>
+ * 
+ * @author lenovo
+ * @version 1.0.0
+ * @see
+ */
+public class SoluationContainerWithMostWater {
+    public int maxArea(int[] height) {
+        int maxarea = 0, l = 0, r = height.length - 1;
+        while (l < r) {
+            maxarea = Math.max(maxarea, Math.min(height[l], height[r]) * (r - l));
+            if (height[l] < height[r])
+                l++;
+            else
+                r--;
+        }
+        return maxarea;
+    }
+}
