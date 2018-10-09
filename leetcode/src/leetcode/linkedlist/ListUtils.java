237e9bde4f068cd61cb933d8c439d8ef90037058
Gilbert Hou <java_hou@163.com>
2018-10-09 20:37:29 +0800

    链表分割

--- leetcode/src/leetcode/linkedlist/ListUtils.java
+++ leetcode/src/leetcode/linkedlist/ListUtils.java
@@ -26,6 +26,11 @@ public class ListUtils {
         ListNode node4 = new ListNode(4);
         ListNode node5 = new ListNode(5);
         ListNode node6 = new ListNode(6);
+        ListNode node7 = new ListNode(2);
+        ListNode node8 = new ListNode(1);
+        ListNode node9 = new ListNode(3);
+        ListNode node10 = new ListNode(1);
+        ListNode node11 = new ListNode(5);
 
         head.next = node2;
         node2.next = node3;
@@ -34,6 +39,11 @@ public class ListUtils {
         node32.next = node4;
         node4.next = node5;
         node5.next = node6;
+        node6.next = node7;
+        node7.next = node8;
+        node8.next = node9;
+        node9.next = node10;
+        node10.next = node11;
         return head;
     }
 
---
+++ leetcode/src/leetcode/linkedlist/SoluationListPartition.java
@@ -0,0 +1,47 @@
+package leetcode.linkedlist;
+
+
+
+public class SoluationListPartition {
+
+    public static void main(String[] args) {
+
+
+        ListNode head = ListUtils.createNewList();
+        System.err.println("Init:");
+        ListUtils.printList(head);
+
+        ListNode newHead = partition(head, 3);
+        System.err.println("After:");
+        ListUtils.printList(newHead);
+
+
+    }
+
+
+    public static ListNode partition(ListNode head, int x) {
+        if (head == null) {
+            return null;
+        }
+        ListNode smallHead = new ListNode(0);
+        ListNode p1 = smallHead;
+        ListNode bigHead =  new ListNode(0);
+        ListNode p2 = bigHead;
+        ListNode current = head;
+
+
+        while (current != null) {
+            if (current.val < x) {
+                    p1.next = current;
+                    p1 = p1.next;
+            } else {
+                    p2.next = current;
+                    p2 = p2.next;
+            }
+            current = current.next;
+        }
+        p1.next = bigHead.next;
+        return smallHead.next;
+    }
+
+}
