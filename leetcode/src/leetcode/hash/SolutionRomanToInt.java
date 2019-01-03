53b00ffc8c2b40fda799828b6190d11b1e56fb1a
Gilbert Hou <java_hou@163.com>
2019-01-03 21:23:51 +0800

    13:罗马数字转整数

---
+++ leetcode/src/leetcode/hash/SolutionRomanToInt.java
@@ -0,0 +1,50 @@
+package leetcode.hash;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SolutionRomanToInt {
+
+
+    public static void main(String[] args) {
+
+        System.err.println(romanToInt("MCMXCIV"));
+
+    }
+
+    public static int romanToInt(String s) {
+
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+
+        int number = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char cur = s.charAt(i);
+                if (i + 1 < s.length()) {
+                    if (map.get(cur) >= map.get(s.charAt(i+1))) {
+                        number += map.get(cur);
+                    } else {
+                        number -= map.get(cur);
+                    }
+
+                } else {
+                    number += map.get(cur);
+                }
+
+
+            }
+        return number;
+    }
+}
+
+
+
+
+
