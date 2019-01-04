df3d3e7e4d4d5fccbed41d674d4d9bac0dbee9be
Gilbert Hou <java_hou@163.com>
2019-01-04 18:57:52 +0800

    12. 整数转罗马数字

---
+++ leetcode/src/leetcode/SolutionIntToRoman.java
@@ -0,0 +1,65 @@
+package leetcode;
+
+public class SolutionIntToRoman {
+
+    public static void main(String[] args) {
+
+        System.err.println(intToRoman(58));
+    }
+
+
+    public static String intToRoman(int num) {
+
+
+        int n = num;
+
+        String s = "";
+
+        while (n > 0) {
+            if (n >= 1000) {
+                s += "M";
+                n -= 1000;
+            } else if (n >= 900) {
+                s += "CM";
+                n -= 900;
+            } else if (n >= 500) {
+                s += "D";
+                n -= 500;
+            } else if (n >= 400) {
+                s += "CD";
+                n -= 400;
+            } else if (n >= 100) {
+                s += "C";
+                n -= 100;
+            } else if (n >= 90) {
+                s += "XC";
+                n -= 90;
+            } else if (n >= 50) {
+                s += "L";
+                n -= 50;
+            } else if (n >= 40) {
+                s += "XL";
+                n -= 40;
+            } else if (n >= 10) {
+                s += "X";
+                n -= 10;
+            } else if (n >= 9) {
+                s += "IX";
+                n -= 9;
+            } else if (n >= 5) {
+                s += "V";
+                n -= 5;
+            } else if (n >= 4) {
+                s += "IV";
+                n -= 4;
+            } else {
+                s += "I";
+                n -= 1;
+            }
+        }
+        return s;
+
+    }
+
+
+}
