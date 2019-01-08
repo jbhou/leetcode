ebe3c952876ea8204324d3957241a262d49c466b
Gilbert Hou <java_hou@163.com>
2019-01-08 19:22:37 +0800

    796. 旋转字符串

---
+++ leetcode/src/leetcode/SolutionRotateString.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+public class SolutionRotateString {
+
+
+    public boolean rotateString(String a, String b) {
+
+        if (a.length() != b.length()) {
+            return false;
+        }
+        if (a.equals(b)) {
+            return true;
+        }
+
+        String s2 = a + a;
+        return s2.contains(b);
+
+
+    }
+
+
+}
