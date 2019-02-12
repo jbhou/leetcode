18fee6fc9b178571b2b59f0eb6e52815dc8e49de
Gilbert Hou <java_hou@163.com>
2019-02-12 20:56:02 +0800

    973 最接近原点的K个点

--- leetcode/src/leetcode/SolutionKClosest.java
+++ leetcode/src/leetcode/SolutionKClosest.java
@@ -6,12 +6,10 @@ public class SolutionKClosest {
 
 
     public static void main(String[] args) {
-
         int[][] a = new int[2][2];
         a[0] = new int[]{1, 3};
         a[1] = new int[]{-2, 2};
         new SolutionKClosest().kClosest(a, 1);
-
     }
 
 
