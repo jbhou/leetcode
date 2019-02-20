f191bd149fa191ad225109a0737706dcee91a81e
Gilbert Hou <java_hou@163.com>
2019-02-20 20:40:16 +0800

    946.验证栈序列

---
+++ leetcode/src/leetcode/SolutionValidateStackSequences.java
@@ -0,0 +1,49 @@
+package leetcode;
+
+import java.util.Stack;
+
+public class SolutionValidateStackSequences {
+
+
+    public static void main(String[] args) {
+
+
+        int[] a = new int[]{1,0};
+        int[] b = new int[]{1,0};
+
+
+        boolean res = new SolutionValidateStackSequences().validateStackSequences(a, b);
+        System.err.println(res
+        );
+    }
+
+
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+// 1 2 3 4 5
+        // 4 5 3 2 1
+
+        Stack<Integer> stack = new Stack<>();
+
+        int j = 0;
+        for (int i = 0; i < pushed.length; i++) {
+
+            stack.push(pushed[i]);
+
+            while (j < popped.length  && !stack.empty() && stack.peek() == popped[j]) {
+                j++;
+                stack.pop();
+            }
+        }
+
+        while (j < popped.length) {
+            if (popped[j++] != stack.pop()) {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+
+
+}
