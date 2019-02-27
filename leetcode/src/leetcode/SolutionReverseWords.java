1d5ed718106d16071b6c6e0089897c4caa6e26de
Gilbert Hou <java_hou@163.com>
2019-02-27 20:46:07 +0800

    151. 翻转字符串里的单词

---
+++ leetcode/src/leetcode/SolutionReverseWords.java
@@ -0,0 +1,46 @@
+package leetcode;
+
+public class SolutionReverseWords {
+
+    public static void main(String[] args) {
+
+        String s = "I am a Student.";
+        System.err.println(new SolutionReverseWords().reverseWords(s));
+
+    }
+
+
+    public String reverseWords(String s) {
+
+        String newS = reverse(s);
+
+
+        String[] splited = newS.split(" ");
+
+        String res = "";
+        for (int i = 0; i < splited.length; i++) {
+            if (!"".equals(splited[i].trim())) {
+                res += reverse(splited[i]) + " ";
+            }
+        }
+
+        return res.substring(0, res.length() - 1);
+
+
+    }
+
+
+    public String reverse(String s) {
+
+
+        String r = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            r += s.charAt(i);
+        }
+        return r;
+
+
+    }
+
+
+}
