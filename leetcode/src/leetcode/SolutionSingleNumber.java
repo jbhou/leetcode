829d08f74b9965c58a7fc35d273e06c9a1fc9712
Gilbert Hou <java_hou@163.com>
2019-03-01 10:54:49 +0800

    136. 只出现一次的数字

---
+++ leetcode/src/leetcode/SolutionSingleNumber.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+public class SolutionSingleNumber {
+
+
+    public int singleNumber(int[] nums) {
+
+
+        int a = nums[0];
+
+
+        for (int i = 1; i < nums.length; i++) {
+            a = a ^ nums[i];
+        }
+        return a;
+
+
+    }
+}
---
+++ leetcode/src/test/Test.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Test {
+
+
+    public static void main(String[] args) {
+        int a = 5;
+
+        System.err.println(a & 3);
+
+
+    }
+}
