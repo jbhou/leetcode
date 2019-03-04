36b8cc0a49509db6ff85f741eb0950de6f3dca65
Gilbert Hou <java_hou@163.com>
2019-03-04 21:49:52 +0800

    235.二叉搜索树的公共祖先

---
+++ leetcode/src/leetcode/tree/SolutionLowestCommonAncestor.java
@@ -0,0 +1,20 @@
+package leetcode.tree;
+
+public class SolutionLowestCommonAncestor {
+
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+
+        if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        return root;
+
+
+    }
+
+
+}
