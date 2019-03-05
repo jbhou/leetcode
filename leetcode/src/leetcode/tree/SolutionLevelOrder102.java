30a8ef6a8915f8a2cc9f24f654cc865d40af10ef
Gilbert Hou <java_hou@163.com>
2019-03-05 22:39:51 +0800

    深度优先搜索

--- leetcode/src/leetcode/tree/SolutionLevelOrder102.java
+++ leetcode/src/leetcode/tree/SolutionLevelOrder102.java
@@ -4,8 +4,14 @@ import sun.reflect.generics.tree.Tree;
 
 import java.util.*;
 
-public class SolutionLevelOrder {
+public class SolutionLevelOrder102 {
 
+    /**
+     * BFS 广度优先搜索
+     *
+     * @param root
+     * @return
+     */
 
     public List<List<Integer>> levelOrder(TreeNode root) {
         if (root == null)
@@ -28,6 +34,39 @@ public class SolutionLevelOrder {
             res.add(list);
         }
         return res;
+    }
+
+
+    /**
+     * 深度优先搜索处理
+     *
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> leverOrderDFS(TreeNode root) {
+
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        dfs(result, root, 0 + 1);
+
+        return result;
+    }
+
+    private void dfs(List<List<Integer>> result, TreeNode root, int level) {
+
+        if (root == null) {
+            return;
+        }
+
+        if (result.size() < level) {
+            result.add(new ArrayList<>());
+        }
+        result.get(level - 1).add(root.val);
+        dfs(result, root.left, level + 1);
+        dfs(result, root.right, level + 1);
+
+
     }
 
 
