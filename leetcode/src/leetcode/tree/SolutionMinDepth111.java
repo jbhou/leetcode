76bb7bd43ed66028a6a8368744c7a47d97071455
Gilbert Hou <java_hou@163.com>
2019-03-07 17:44:08 +0800

    111. 二叉树的最小深度

---
+++ leetcode/src/leetcode/tree/SolutionMinDepth111.java
@@ -0,0 +1,27 @@
+package leetcode.tree;
+
+public class SolutionMinDepth111 {
+
+
+
+
+
+
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftDepth = minDepth(root.left);
+        int rightDepth = minDepth(root.right);
+
+        return leftDepth==0 || rightDepth==0 ? leftDepth+rightDepth+1:Math.min(leftDepth,rightDepth)+1;
+
+
+    }
+
+
+
+
+
+
+}
