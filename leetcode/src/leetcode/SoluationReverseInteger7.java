482f2d8c1823e4a54fa837449dcdc0c7b084636e
Gilbert Hou <java_hou@163.com>
2019-03-11 18:56:28 +0800

    7.整数反转

--- leetcode/src/leetcode/SoluationReverseInteger7.java
+++ leetcode/src/leetcode/SoluationReverseInteger7.java
@@ -3,9 +3,9 @@
  * file distributed with this work for additional information regarding copyright ownership. The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
  * License. You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
@@ -17,12 +17,12 @@ package leetcode;
  * Function: TODO ADD FUNCTION. <br/>
  * Reason: TODO ADD REASON. <br/>
  * Date: 2018年5月24日 下午7:47:54 <br/>
- * 
+ *
  * @author lenovo
  * @version 1.0.0
  * @see
  */
-public class SoluationReverseInteger {
+public class SoluationReverseInteger7 {
     public int reverse(int x) {
         String str = String.valueOf(x);
         String newStr = "";
@@ -33,14 +33,39 @@ public class SoluationReverseInteger {
         }
         Long val = Long.parseLong(newStr);
         val = x < 0 ? val * -1 : val;
-        if(val > Integer.MAX_VALUE || val < Integer.MIN_VALUE){
+        if (val > Integer.MAX_VALUE || val < Integer.MIN_VALUE) {
             return 0;
         }
         return val.intValue();
     }
 
     public static void main(String[] args) {
-        int v = new SoluationReverseInteger().reverse(1534236469);
+        int v = new SoluationReverseInteger7().reverse2(213);
         System.err.println(v);
     }
+
+
+    public int reverse2(int x) {
+
+
+        int m = Math.abs(x);
+        long r = 0;
+
+
+        while (m != 0) {
+            r = (r * 10) + (m % 10);
+            m = m / 10;
+        }
+
+        if (x < 0) {
+            r = r * -1;
+        }
+
+        if(r>Integer.MAX_VALUE || r<Integer.MIN_VALUE) {
+            return 0;
+        }
+        return (int) r;
+    }
+
+
 }
