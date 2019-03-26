748be549b8449d4d9e34530ac143e985a477da6a
Gilbert Hou <java_hou@163.com>
2019-03-26 16:39:52 +0800

    70.动态规划爬楼梯

--- leetcode/src/leetcode/SolutionClimbStairs70.java
+++ leetcode/src/leetcode/SolutionClimbStairs70.java
@@ -10,13 +10,36 @@ public class SolutionClimbStairs70 {
 
     }
 
+    //冬天规划
     public int climbStairs(int n) {
 
-        int[] cache = new int[n + 1];
-        return climbStairs(n, cache);
+
+        if (n <= 2) return n;
+
+        int x = 1;
+        int y = 2;
+        int z = 0;
+        for (int i = 2; i < n; i++) {
+
+            z = x + y;
+            x = y;
+            y = z;
+        }
+        return z;
+
+//        int[] cache = new int[n + 1];
+//        return climbStairs(n, cache);
 
     }
 
+
+    /**
+     * 递归回溯
+     *
+     * @param n
+     * @param cache
+     * @return
+     */
 
     public int climbStairs(int n, int[] cache) {
 
