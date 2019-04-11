7a37687dbd125ecd1efcffa15c7e435e54fa3c17
Gilbert Hou <java_hou@163.com>
2019-04-11 21:30:24 +0800

    45.跳跃次数

---
+++ leetcode/src/GeekSolutionJump45.java
@@ -0,0 +1,57 @@
+public class GeekSolutionJump45 {
+
+
+    public static void main(String[] args) {
+
+
+        int m = new GeekSolutionJump45().jump(new int[]{2, 3, 1, 1, 4});
+        System.err.println(m);
+
+    }
+
+
+    int min = Integer.MAX_VALUE;
+
+    //https://blog.csdn.net/MebiuW/article/details/51171153
+    public int jump(int[] a) {
+
+
+        int times = 0;
+        int reached = 0;
+        int max = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (reached < i) {
+                times++;
+                reached = max;
+            }
+            max = Math.max(max, i + a[i]);
+        }
+        return times;
+    }
+
+
+    /**
+     * 超时
+     *
+     * @param a
+     * @param currentIndex
+     * @param preStep
+     * @param totalSteps
+     */
+    private void jumpTo(int[] a, int currentIndex, int preStep, int totalSteps) {
+
+
+        if (currentIndex >= a.length - 1) {
+            min = min > totalSteps ? totalSteps : min;
+            return;
+        }
+
+        for (int i = preStep + 1; i <= a[currentIndex]; i++) {
+            jumpTo(a, currentIndex + i, preStep, totalSteps + 1);
+        }
+
+
+    }
+
+
+}
