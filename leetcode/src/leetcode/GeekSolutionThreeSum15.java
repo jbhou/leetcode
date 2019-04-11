9c17f53aa29c45f05cab119584883b90dca70a48
Gilbert Hou <java_hou@163.com>
2019-04-11 19:31:23 +0800

    15.三数之和

--- leetcode/src/leetcode/GeekSolutionThreeSum15.java
+++ leetcode/src/leetcode/GeekSolutionThreeSum15.java
@@ -1,15 +1,3 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements. See the NOTICE
- * file distributed with this work for additional information regarding copyright ownership. The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
- */
 package leetcode;
 
 import java.util.ArrayList;
@@ -23,14 +11,12 @@ import java.util.Set;
  * Function: TODO ADD FUNCTION. <br/>
  * Reason: TODO ADD REASON. <br/>
  * Date: 2018年5月26日 下午12:21:51 <br/>
- * 
+ *
  * @author lenovo
  * @version 1.0.0
  * @see
  */
-public class SolutionThreeSum15 {
-
-
+public class GeekSolutionThreeSum15 {
 
 
     public List<List<Integer>> threeSum(int[] nums) {
@@ -51,12 +37,37 @@ public class SolutionThreeSum15 {
         return list;
     }
 
+
+    public List<List<Integer>> threeSum2(int[] nums) {
+
+
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (i >= 1 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            Set<Integer> set = new HashSet<>();
+            for (int j = i + 1; j < nums.length; j++) {
+                if (set.contains(nums[j])) {
+                    result.add(Arrays.asList(nums[i], -nums[i] - nums[j], nums[j]));
+                } else {
+                    set.add(-nums[i] - nums[j]);
+                }
+            }
+        }
+//        return result;
+
+        return new ArrayList<>(new HashSet<>(result));
+    }
+
+
     /**
      * 超时 main:(这里用一句话描述这个方法的作用). <br/>
      * TODO(这里描述这个方法适用条件 – 可选).<br/>
      *
-     * @author lenovo
      * @param args
+     * @author lenovo
      */
     // public List<List<Integer>> threeSum(int[] nums) {
     //
@@ -88,7 +99,7 @@ public class SolutionThreeSum15 {
     // return res;
     // }
     public static void main(String[] args) {
-        List<List<Integer>> r = new SolutionThreeSum15().threeSum(new int[] { -1, 0, 1, 2, -1, -4 });
+        List<List<Integer>> r = new GeekSolutionThreeSum15().threeSum2(new int[]{0,0,0,0});
         System.err.println(r);
     }
 }
