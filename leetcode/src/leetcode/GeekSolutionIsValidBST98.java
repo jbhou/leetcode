b740d2be37a7e3f2a639b8137304a27b608dbf01
Gilbert Hou <java_hou@163.com>
2019-04-12 11:26:50 +0800

    98.验证 二叉搜索树

---
+++ leetcode/src/leetcode/GeekSolutionIsValidBST98.java
@@ -0,0 +1,58 @@
+package leetcode;
+
+import leetcode.tree.TreeNode;
+
+public class GeekSolutionIsValidBST98 {
+
+
+    private TreeNode preNode;
+
+
+    public boolean isValidBST2(TreeNode root) {
+
+
+        return inOrderValid(root);
+    }
+
+    private boolean inOrderValid(TreeNode root) {
+
+        if (root == null) {
+            return true;
+        }
+        if (!inOrderValid(root.left)) {
+            return false;
+        }
+        if (preNode != null && root.val <= preNode.val) {
+            return false;
+        }
+        preNode = root;
+        return inOrderValid(root.right);
+    }
+
+
+    /**
+     * 递归
+     *
+     * @param root
+     * @return
+     */
+
+    public boolean isValidBST(TreeNode root) {
+
+        return validate(root, null, null);
+
+    }
+
+
+    private boolean validate(TreeNode root, Integer min, Integer max) {
+
+
+        if (root == null) {
+            return true;
+        }
+
+        if ((min != null && min > root.val) || (max != null && max < root.val)) return false;
+        return validate(root.left, min, root.val) && validate(root.right, root.val, max);
+
+    }
+}
