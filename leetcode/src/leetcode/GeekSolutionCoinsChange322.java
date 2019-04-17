bfb9d5f93b4cc48199c1b2fa15edf421025b7d38
Gilbert Hou <java_hou@163.com>
2019-04-17 10:00:34 +0800

    零钱兑换

---
+++ leetcode/src/leetcode/GeekSolutionCoinsChange322.java
@@ -0,0 +1,43 @@
+package leetcode;
+
+public class GeekSolutionCoinsChange322 {
+
+
+//
+//    给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回 -1。
+//
+//    示例 1:
+//
+//    输入: coins = [1, 2, 5], amount = 11
+//    输出: 3
+//    解释: 11 = 5 + 5 + 1
+//    示例 2:
+//
+//    输入: coins = [2], amount = 3
+//    输出: -1
+//    说明:
+//    你可以认为每种硬币的数量是无限的。
+
+    public int coinChange(int[] coins, int amount) {
+
+
+        int[] dp = new int[amount + 1]; // 每个元素的含义是 i 金额下所需要的硬币数量
+
+        dp[0] = 0;
+
+        for (int i = 1; i <= amount; i++) {
+
+            dp[i] = amount + 1;
+            for (int j = 0; j < coins.length; j++) {
+                if (i >= coins[j])
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+
+            }
+        }
+        return dp[amount] > amount ? -1: dp[amount];
+    }
+
+    public static void main(String[] args) {
+        new GeekSolutionCoinsChange322().coinChange(new int[]{2}, 3);
+    }
+}
